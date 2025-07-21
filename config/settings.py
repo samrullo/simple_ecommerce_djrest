57ef1321d4e8bed154a5c5af048e6e8d1b51d050
Subkhon Amrulloev <amrulloev.subhon@gmail.com>
2025-07-22 07:11:18 +0900

    change settings to use postgres, add render.yaml that should run migration

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["simple-ecommerce-djrest.onrender.com"]
 
 # At the bottom of your settings.py file (after INSTALLED_APPS, etc.)
 # DJ_REST_AUTH = {
@@ -137,23 +137,23 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_DB'),
-#         'USER': os.environ.get('POSTGRES_USER'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#         'HOST': os.environ.get('POSTGRES_HOSTNAME'),  # Set to the address of your PostgreSQL instance if not on the same machine.
-#         'PORT': os.environ.get('POSTGRES_PORT'),  # Default PostgreSQL port.
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOSTNAME'),  # Set to the address of your PostgreSQL instance if not on the same machine.
+        'PORT': os.environ.get('POSTGRES_PORT'),  # Default PostgreSQL port.
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
